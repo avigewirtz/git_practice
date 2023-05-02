2440e562b10e43b6eda9fffce0630831ef3a44a8
avigewirtz <131504020+avigewirtz@users.noreply.github.com>
2023-05-02 14:48:20 -0400

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+//----------------------------------------------------------------------
+// Hello.java                                                         
+//----------------------------------------------------------------------
+
+public class Hello
+{
+   // Write "hello, world\n" to the standard output stream.
+
+   public static void main(String[] args)
+   {
+      System.out.println("hello, world");
+   }
+}
